b53e3a3304816a6bc55125cc755cd30fe820842b
laurilauter <lauri.lauter@gmail.com>
2022-12-30 16:54:07 +0200

    Port changed again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 80;
 app.use(express.json());
+const port = process.env.PORT;
 const mongoose = require("mongoose");
 require("dotenv").config();
 const LOCAL_URI = "mongodb://localhost:27017/node-test"; //for local debuging
