28d374001dc69f8546c6cf22af0dbfa52ad63c84
laurilauter <lauri.lauter@gmail.com>
2022-12-30 14:35:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+const mongoose = require("mongoose");
+require("dotenv").config();
+//const config = JSON.parse(process.env.APP_CONFIG);
+const mongoPassword = process.env.MONGO_PASSWORD;
+const DEV_URI = "mongodb://localhost:27017/node-test"; //for local debuging
+
+const Schema = mongoose.Schema;
+
+const namesSchema = new Schema({
+  name: String,
+});
+
+const Names = mongoose.model("Names", namesSchema);
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.PROD_URI)
+  .then(() => {
+    console.log("Successfully connected to the DB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Node server root" });
+});
+
+app.post("/data", async (req, res) => {
+  console.log("req.body ", req.body);
+  try {
+    const name = await Names.create({
+      name: req.body.name,
+    });
+    res.status(200).send(name);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+app.get("/data", async (req, res) => {
+  try {
+    let list = await Names.find();
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at: http://localhost:${port}`);
+});
