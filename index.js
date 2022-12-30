cb868c7399ece827457502f7ad863b665bb7c934
laurilauter <lauri.lauter@gmail.com>
2022-12-30 20:37:46 +0200

    Added front2

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT;
 const mongoose = require("mongoose");
 require("dotenv").config();
 const LOCAL_URI = "mongodb://localhost:27017/node-test"; //for local debugging
-import path from "path";
-import { fileURLToPath } from "url";
+const path = require("path");
+const { fileURLToPath } = require("url");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
