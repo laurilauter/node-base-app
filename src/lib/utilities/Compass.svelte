780270074c7c111baabc1646c65366526d9bd057
laurilauter <lauri.lauter@gmail.com>
2023-03-12 15:06:45 +0200

    Compass added4

--- src/lib/utilities/Compass.svelte
+++ src/lib/utilities/Compass.svelte
@@ -4,24 +4,19 @@
   import Navigation from "svelte-material-icons/Navigation.svelte";
 
   let orientation;
+  let absolute;
 
   function handleOrientation(event) {
     orientation = Math.trunc(event.alpha);
+    absolute = event.absolute;
   }
   window.addEventListener("deviceorientation", handleOrientation);
 </script>
 
 <div on:deviceorientation={handleOrientation}>
-  <span>Device orientation: {orientation}</span>
-  <Navigation
-    size={"2rem"}
-    ariaHidden={false}
-    horizontal
-    rotate={orientation}
-    color={"#ff0033"}
-  />
+  <span>Device orientation: {orientation} absolute: {absolute}</span>
   <div style="rotate: {orientation}deg;">
-    <Navigation size={"2rem"} ariaHidden={false} horizontal color={"#ff0099"} />
+    <Navigation size={"2rem"} ariaHidden={false} horizontal color={"#ff0033"} />
   </div>
 </div>
 
